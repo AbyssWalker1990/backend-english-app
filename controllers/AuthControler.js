12b8776522a9b56b100d10512e134c24310a7313
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-07-14 22:10:05 +0300

    added login user route

--- controllers/AuthControler.js
+++ controllers/AuthControler.js
@@ -2,6 +2,7 @@ const express = require('express')
 const loginLimiter = require('../middleware/loginLimiter')
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 class AuthController {
   path = '/auth'
@@ -13,6 +14,7 @@ class AuthController {
 
   initRoutes () {
     this.router.post(`${this.path}/register`, this.register)
+    this.router.post(`${this.path}/login`, this.login)
   }
 
   register = async (req, res, next) => {
@@ -43,6 +45,52 @@ class AuthController {
     } else {
       res.status(400).json({ message: 'Invalid user data received' })
     }
+  }
+
+  login = async (req, res, next) => {
+    const { username, password } = req.body
+
+    if (!username || !password) {
+      return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    const foundUser = await User.findOne({ username }).exec()
+
+    if (!foundUser || !foundUser.active) {
+      return res.status(401).json({ message: 'Unauthorized' })
+    }
+
+    const match = await bcrypt.compare(password, foundUser.password)
+
+    if (!match) return res.status(401).json({ message: 'Unauthorized' })
+
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          username: foundUser.username,
+          roles: foundUser.roles
+        }
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '15m' }
+    )
+
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '7d' }
+    )
+
+    // Create secure cookie with refresh token
+    res.cookie('jwt', refreshToken, {
+      httpOnly: true, // accessible only by web server
+      secure: true, // https
+      sameSite: 'None', // cross-site cookie
+      maxAge: 7 * 24 * 60 * 60 * 1000 // cookie expiry: set to match rT
+    })
+
+    // Send accessToken containing username and roles
+    res.json({ accessToken })
   }
 }
 
