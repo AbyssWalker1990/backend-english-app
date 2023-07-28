afe14dffad08fd935ecf5212eeb20e3bb4654df3
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-07-28 13:02:44 +0300

    added email to user model and simple validation for duplicates

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -20,24 +20,28 @@ class AuthController {
   }
 
   register = async (req, res, next) => {
-    const { username, password, roles } = req.body
+    const { username, password, roles, email } = req.body
     const currentRoles = roles ?? ['User']
     // Confirm data
-    if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    if (!username || !password || !email || !Array.isArray(roles) || !roles.length) {
       return res.status(400).json({ message: 'All fields are required' })
     }
 
     // Check for duplicate username
-    const duplicate = await User.findOne({ username }).lean().exec()
-
-    if (duplicate) {
+    const duplicateUser = await User.findOne({ username }).lean().exec()
+    if (duplicateUser) {
       return res.status(409).json({ message: 'Duplicate username' })
+    }
+
+    const duplicateEmail = await User.findOne({ email }).lean().exec()
+    if (duplicateEmail) {
+      return res.status(409).json({ message: 'Duplicate email' })
     }
 
     // Hash password
     const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
 
-    const userObject = { username, password: hashedPwd, roles: currentRoles }
+    const userObject = { username, password: hashedPwd, email, roles: currentRoles }
 
     // Create and store new user
     const user = await User.create(userObject)
