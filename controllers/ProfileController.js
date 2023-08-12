94d48c8748031b2af41d7414620cbc9681d49439
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-08-12 15:26:19 +0300

    refactored calculateLessonResult

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -1,7 +1,5 @@
-const dotenv = require('dotenv')
 const express = require('express')
 const User = require('../models/User')
-const Course = require('../models/Course')
 const verifyJWT = require('../middleware/verifyJWT')
 const upload = require('../middleware/multer')
 const ProfileService = require('../services/ProfileService')
@@ -43,118 +41,22 @@ class ProfileController {
     res.status(200).json({ photo, objectives, priorities, hobbies, courses, activeCourse, coursesAnswers })
   }
 
-  setAnswers = async (req, res) => {
-    const { courseId, lessonPosition, blockPosition, exerciseAnswers } = req.body
-    const currentUser = await User.findOne({ username: req.user })
-    console.log('lessonPosition: ', lessonPosition)
-
-    if (currentUser.profile.coursesAnswers.find(answer => answer.courseId === courseId) === undefined) {
-      await this.setInitAnswers(courseId, req.username)
-    }
-
-    console.log('do nothing')
-
+  setAnswers = async (req, res, next) => {
     try {
-      const currentUser3 = await User.findOne({ username: req.user })
-      console.log(exerciseAnswers)
-      console.log(Array.isArray(exerciseAnswers))
-
-      if (currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
-        .lessons.find(lesson => lesson.lessonPosition === lessonPosition) === undefined) {
-        console.log('creating Lesson')
-        currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
-          .lessons.push({
-            lessonPosition
-          })
-      }
-
-      if (currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
-        .lessons.find(lesson => lesson.lessonPosition === lessonPosition)
-        .exercisesBlocks.find(block => block.blockPosition === blockPosition) !== undefined) {
-        console.log('triggered if')
-        currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
-          .lessons.find(lesson => lesson.lessonPosition === lessonPosition)
-          .exercisesBlocks.find(block => block.blockPosition === blockPosition).blockExercises = exerciseAnswers
-        currentUser3.save()
-      } else {
-        const newBlock = { blockPosition, blockExercises: exerciseAnswers }
-        currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
-          .lessons.find(lesson => lesson.lessonPosition === lessonPosition).exercisesBlocks.push(newBlock)
-        currentUser3.save()
-      }
+      const isUpdated = await this.profileService.updateAnswers(req.user, req.body)
+      res.status(200).json(isUpdated)
     } catch (error) {
-      console.log('Error from received data: ', error)
+      next(error)
     }
   }
 
-  calculateLessonResult = async (req, res) => {
-    const currentUser = await User.findOne({ username: req.user }).exec()
-    const { courseId, lessonPos } = req.body
-    console.log('calculateLessonResult Triggered')
-    const curLesson = await this.getLessonByPosition(courseId, lessonPos)
-    const curLessonAnswers = await this.getLessonAnswersByPosition(courseId, lessonPos, currentUser)
-    // console.log('curLesson: ', curLesson)
-    // console.log('curLessonAnswers: ', curLessonAnswers)
-
-    // console.log('curLessonAnswers: ', curLessonAnswers)
-
-    let lessonResult = JSON.parse(JSON.stringify(curLessonAnswers))
-    lessonResult = {
-      ...lessonResult,
-      lessonResultPercent: 0,
-      lessonResultRight: 0,
-      lessonResultWrong: 0
+  calculateLessonResult = async (req, res, next) => {
+    try {
+      const curLesson = await this.profileService.updateResult(req.user, req.body)
+      res.status(200).json({ curLesson, success: true })
+    } catch (error) {
+      next(error)
     }
-
-    let lessonResultRight = 0
-    let lessonResultWrong = 0
-    let exerciseCount = 0
-    lessonResult.exercisesBlocks.forEach((block, blockIndex) => {
-      block.blockExercises.forEach((exercise, exerciseIndex) => {
-        exerciseCount += 1
-        const blockRightAnswer = curLesson.exercisesBlocks[blockIndex]
-        const correctAnswer = blockRightAnswer.blockExercises[exerciseIndex].correctAnswer
-        console.log('correctAnswer: ', correctAnswer)
-        exercise.exerciseResult = exercise.studentsAnswer.toLowerCase() === correctAnswer.toLowerCase()
-      })
-      block.blockResultRight = block.blockExercises.reduce((acc, exercise) => acc + exercise.exerciseResult, 0)
-      lessonResultRight += block.blockResultRight
-      console.log('lessonResultRight: ', lessonResultRight)
-      block.blockResultWrong = block.blockExercises.length - block.blockResultRight
-      lessonResultWrong += block.blockResultWrong
-      block.blockResultPercent = block.blockResultRight / block.blockExercises.length * 100
-    })
-    lessonResult.lessonResultRight = lessonResultRight
-    lessonResult.lessonResultWrong = lessonResultWrong
-    lessonResult.lessonResultPercent = lessonResultRight / exerciseCount * 100
-
-    if (currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId).courseResults === undefined) {
-      currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId).courseResults = []
-    }
-
-    const resultsWithoutPrevios = currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId).courseResults.filter(result => {
-      return result.lessonPosition !== Number(lessonPos)
-    })
-    console.log('resultsWithoutPrevios: ', resultsWithoutPrevios)
-
-    currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId).courseResults = [...resultsWithoutPrevios, lessonResult]
-    await currentUser.save()
-
-    // console.log('result: ', JSON.stringify(lessonResult, 0, 2))
-    console.log('res: ', currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId))
-    res.status(200).json({ curLesson, success: true })
-  }
-
-  getLessonByPosition = async (courseId, lessonPos) => {
-    const curCourse = await Course.findById(courseId).exec()
-    const curLesson = curCourse.lessons.find(lesson => lesson.lessonPosition === Number(lessonPos))
-    return curLesson
-  }
-
-  getLessonAnswersByPosition = async (courseId, lessonPos, currentUser) => {
-    const courseAnswers = await currentUser.profile.coursesAnswers.find(course => course.courseId === courseId)
-    const lessonAnswers = courseAnswers.lessons.find(lesson => lesson.lessonPosition === Number(lessonPos))
-    return lessonAnswers
   }
 }
 
--- services/ProfileService.js
+++ services/ProfileService.js
@@ -1,6 +1,5 @@
 const User = require('../models/User')
 const Course = require('../models/Course')
-
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 const s3Config = require('../config/s3Config')
 const HttpException = require('../exceptions/HttpException')
@@ -42,6 +41,114 @@ class ProfileService {
     return true
   }
 
+  updateAnswers = async (username, answersData) => {
+    console.log('trigger updateAnswers')
+    const { courseId, lessonPosition, blockPosition, exerciseAnswers } = answersData
+    const currentUser = await User.findOne({ username })
+    if (currentUser.profile.coursesAnswers.find(answer => answer.courseId === courseId) === undefined) {
+      await this.setInitAnswers(courseId, username)
+    }
+
+    try {
+      const currentUser3 = await User.findOne({ username })
+      console.log(exerciseAnswers)
+      console.log(Array.isArray(exerciseAnswers))
+
+      if (currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
+        .lessons.find(lesson => lesson.lessonPosition === lessonPosition) === undefined) {
+        currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
+          .lessons.push({
+            lessonPosition
+          })
+      }
+
+      if (currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
+        .lessons.find(lesson => lesson.lessonPosition === lessonPosition)
+        .exercisesBlocks.find(block => block.blockPosition === blockPosition) !== undefined) {
+        currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
+          .lessons.find(lesson => lesson.lessonPosition === lessonPosition)
+          .exercisesBlocks.find(block => block.blockPosition === blockPosition).blockExercises = exerciseAnswers
+        currentUser3.save()
+      } else {
+        const newBlock = { blockPosition, blockExercises: exerciseAnswers }
+        currentUser3.profile.coursesAnswers.find(course => course.courseId === courseId)
+          .lessons.find(lesson => lesson.lessonPosition === lessonPosition).exercisesBlocks.push(newBlock)
+        currentUser3.save()
+      }
+    } catch (error) {
+      console.log('Error from received data: ', error)
+    }
+  }
+
+  updateResult = async (username, lessonData) => {
+    const currentUser = await User.findOne({ username }).exec()
+    const { courseId, lessonPos } = lessonData
+    const curLesson = await this.getLessonByPosition(courseId, lessonPos)
+    const curLessonAnswers = await this.getLessonAnswersByPosition(courseId, lessonPos, currentUser)
+    const blankAnswers = this.generateBlankLessonResult(curLessonAnswers)
+    const lessonResult = this.calculateAndAddLessonResults(blankAnswers, curLesson)
+
+    if (currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId).courseResults === undefined) {
+      currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId).courseResults = []
+    }
+
+    const resultsWithoutPrevios = currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId).courseResults.filter(result => {
+      return result.lessonPosition !== Number(lessonPos)
+    })
+
+    currentUser.profile.coursesAnswers.find(answers => answers.courseId === courseId).courseResults = [...resultsWithoutPrevios, lessonResult]
+    await currentUser.save()
+    return curLesson
+  }
+
+  getLessonByPosition = async (courseId, lessonPos) => {
+    const curCourse = await Course.findById(courseId).exec()
+    const curLesson = curCourse.lessons.find(lesson => lesson.lessonPosition === Number(lessonPos))
+    return curLesson
+  }
+
+  getLessonAnswersByPosition = async (courseId, lessonPos, currentUser) => {
+    const courseAnswers = await currentUser.profile.coursesAnswers.find(course => course.courseId === courseId)
+    const lessonAnswers = courseAnswers.lessons.find(lesson => lesson.lessonPosition === Number(lessonPos))
+    return lessonAnswers
+  }
+
+  generateBlankLessonResult = (curLessonAnswers) => {
+    let blankLessonResult = JSON.parse(JSON.stringify(curLessonAnswers))
+    blankLessonResult = {
+      ...blankLessonResult,
+      lessonResultPercent: 0,
+      lessonResultRight: 0,
+      lessonResultWrong: 0
+    }
+    return blankLessonResult
+  }
+
+  calculateAndAddLessonResults = (lessonResult, curLesson) => {
+    let lessonResultRight = 0
+    let lessonResultWrong = 0
+    let exerciseCount = 0
+    lessonResult.exercisesBlocks.forEach((block, blockIndex) => {
+      block.blockExercises.forEach((exercise, exerciseIndex) => {
+        exerciseCount += 1
+        const blockRightAnswer = curLesson.exercisesBlocks[blockIndex]
+        const correctAnswer = blockRightAnswer.blockExercises[exerciseIndex].correctAnswer
+        console.log('correctAnswer: ', correctAnswer)
+        exercise.exerciseResult = exercise.studentsAnswer.toLowerCase() === correctAnswer.toLowerCase()
+      })
+      block.blockResultRight = block.blockExercises.reduce((acc, exercise) => acc + exercise.exerciseResult, 0)
+      lessonResultRight += block.blockResultRight
+      console.log('lessonResultRight: ', lessonResultRight)
+      block.blockResultWrong = block.blockExercises.length - block.blockResultRight
+      lessonResultWrong += block.blockResultWrong
+      block.blockResultPercent = block.blockResultRight / block.blockExercises.length * 100
+    })
+    lessonResult.lessonResultRight = lessonResultRight
+    lessonResult.lessonResultWrong = lessonResultWrong
+    lessonResult.lessonResultPercent = lessonResultRight / exerciseCount * 100
+    return lessonResult
+  }
+
   generatePhotoName = (photo) => {
     const ext = photo.mimetype.split('/')[1]
     const name = photo.originalname.split('.')[0]
@@ -73,7 +180,6 @@ class ProfileService {
   }
 
   buildCourseAnswersStucture = (course) => {
-    console.log('Triggered buildCourseAnswersStucture')
     const lessonAnswersData = course.lessons.map(lesson => {
       return { lessonPosition: lesson.lessonPosition }
     })
