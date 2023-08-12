021cfc80d739a0e758367a8673143df112f4f286
Volodymyr Bordiian <bordiian.v@gmail.com>
2023-08-12 18:13:25 +0300

    Fixed problem with redundant Controller in server.js

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ dotenv.config()
 const App = require('./app')
 const { connectDatabase } = require('./config/connectDatabase')
 const AuthController = require('./controllers/AuthController')
-const TestController = require('./controllers/TestController')
 const CourseController = require('./controllers/CourseController')
 const ProfileController = require('./controllers/ProfileController')
 
 const PORT = process.env.PORT ?? 3501
 const app = new App([
   new AuthController(),
-  new TestController(),
   new CourseController(),
   new ProfileController()
 ], PORT)
