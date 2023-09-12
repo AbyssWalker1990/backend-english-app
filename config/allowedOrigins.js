f047c7846c3a135f7e838a124a29d5fa6e2d5361
Volodymyr Bordiian <volodymyr_bordiyan@dev.abnk.uk>
2023-09-12 11:48:15 +0300

    added CORS origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   'https://64c7d5067a450d1498e25482--bright-bunny-20b68d.netlify.app',
   'https://bright-bunny-20b68d.netlify.app',
   'https://main--bright-bunny-20b68d.netlify.app/',
+  'https://e-w-s.netlify.app',
   '*.netlify.app'
 ]
 
